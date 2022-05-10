8a30c21df9da9251779058eaf693584e462a7f7a
ylehilds <lehi_alcantara@byu.edu>
2022-05-09 19:37:46 -0600

    updated implementation

--- Resources/labs/ch-3/followAlong/http-web-server/serverNodeCore2.js
+++ Resources/labs/ch-3/followAlong/http-web-server/serverNodeCore2.js
@@ -1,22 +1,52 @@
 'use strict'
+const url = require('url')
 const http = require('http')
 const PORT = process.env.PORT || 3000
+const { STATUS_CODES } = http
 
 const hello = `<html>
   <head>
     <style>
      body { background: #333; margin: 1.25rem }
      h1 { color: #EEE; font-family: sans-serif }
-    </style>
+   </style>
   </head>
   <body>
     <h1>Hello World</h1>
   </body>
 </html>`
 
-const serverNodeCore1 = http.createServer((req, res) => {
+const root = `<html>
+<head>
+  <style>
+   body { background: #333; margin: 1.25rem }
+   a { color: yellow; font-size: 2rem; font-family: sans-serif }
+  </style>
+</head>
+<body>
+  <a href='/hello'>Hello</a>
+</body>
+</html>
+`
+
+const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html')
-  res.end(hello)
+  if (req.method !== 'GET') {
+    res.statusCode = 405
+    res.end(STATUS_CODES[res.statusCode] + '\r\n')
+    return
+  }
+  const { pathname } = url.parse(req.url)
+  if (pathname === '/') {
+    res.end(root)
+    return
+  }
+  if (pathname === '/hello') {
+    res.end(hello)
+    return
+  }
+  res.statusCode = 404
+  res.end(STATUS_CODES[res.statusCode] + '\r\n')
 })
 
-serverNodeCore1.listen(PORT)
+server.listen(PORT)
