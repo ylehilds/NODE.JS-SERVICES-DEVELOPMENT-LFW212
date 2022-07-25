02a8c7096b66e95eb139c1b623ed74dbd5b97a7b
ylehilds <lehi_alcantara@byu.edu>
2022-07-24 19:41:06 -0600

    added some terminal commands.

--- Resources/labs/ch-5/followAlong/my-service/routes/bicycle/index.js
+++ Resources/labs/ch-5/followAlong/my-service/routes/bicycle/index.js
@@ -55,3 +55,12 @@ module.exports = async (fastify, opts) => {
     }
   })
 }
+
+// how to check the service:
+
+// node -e "http.get('http://localhost:3000/bicycle/1', ({headers}) => console.log(headers))"
+// node -e "http.get('http://localhost:3000/bicycle/1', ({statusCode}) =>  console.log(statusCode))"
+// node -e "http.get('http://localhost:3000/bicycle/9', ({statusCode}) => console.log(statusCode))"
+// node -e "http.request('http://localhost:3000/bicycle/1', { method: 'post'}, ({statusCode}) => console.log(statusCode)).end()"
+// node -e "http.get('http://localhost:3000/bicycle/1', ({statusCode}) => console.log(statusCode))"
+
