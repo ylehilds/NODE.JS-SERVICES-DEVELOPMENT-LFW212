6db6267bd29aee50abb4df85117ba6f605c1c8c5
ylehilds <lehi_alcantara@byu.edu>
2022-08-12 10:17:04 -0600

    added lab steps

---
+++ Resources/labs/ch-4/labs-2/steps.js
@@ -0,0 +1,8 @@
+// #1 install fastify: npm init fastify
+// #2 install dependencies: npm i
+// #3 create a data router/rename example to data
+// #3 import stream file and return it like this:
+
+const stream = require('../../stream')
+...
+return await stream()
