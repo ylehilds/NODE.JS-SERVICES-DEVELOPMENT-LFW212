2fc1e3d1d767bebc5b0a428a8db5997a619381bc
ylehilds <lehi_alcantara@byu.edu>
2022-08-14 23:06:39 -0600

    removed duplicate stream.js under util folder. Removed await from response as fastify knows how to work with streams and keep them opened until done.

--- Resources/labs/ch-4/labs-2/routes/data/index.js
+++ Resources/labs/ch-4/labs-2/routes/data/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const stream = require('../../util/stream')
+const stream = require('../../stream')
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return reply.code(200).send( await stream())
+    return reply.code(200).send(stream())
   })
 }
