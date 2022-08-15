b4b699b3c94951beacf3d8012d783989a559abbd
ylehilds <lehi_alcantara@byu.edu>
2022-08-14 23:13:00 -0600

    added instructions and modified the response back to match our convention.

--- Resources/labs/ch-4/labs-2/steps.js
+++ Resources/labs/ch-4/labs-2/steps.js
@@ -1,8 +1,8 @@
 // #1 install fastify: npm init fastify
 // #2 install dependencies: npm i
 // #3 create a data router/rename example to data
-// #3 import stream file and return it like this:
+// #3 import stream file and return it like this, noticed there is no await, that is because fastify knows how to work with streams and keeps connection opened until the stream gets the close event:
 
 const stream = require('../../stream')
 ...
-return stream()
+return reply.code(200).send(stream())
