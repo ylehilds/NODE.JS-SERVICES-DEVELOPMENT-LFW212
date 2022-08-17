685d4b7cdc6b363a76c00c0af5d1e3bef603910c
ylehilds <lehi_alcantara@byu.edu>
2022-08-16 23:52:49 -0600

    refactored ch-7/labs-1

--- Resources/labs/ch-7/labs-1/routes/root.js
+++ Resources/labs/ch-7/labs-1/routes/root.js
@@ -2,14 +2,13 @@
 const got = require('got')
 
 const {
-  BOAT_SERVICE_PORT = 4000, BRAND_SERVICE_PORT = 5001
+  BOAT_SERVICE_PORT, BRAND_SERVICE_PORT
 } = process.env
 
 const boatSrv = `http://localhost:${BOAT_SERVICE_PORT}`
 const brandSrv = `http://localhost:${BRAND_SERVICE_PORT}`
 
 module.exports = async function (fastify, opts) {
-  const { httpErrors } = fastify
   fastify.get('/:id', async function (request, reply) {
     const { id } = request.params
     try {
@@ -18,17 +17,13 @@ module.exports = async function (fastify, opts) {
       return {
         id: boat.id,
         color: boat.color,
-        brand: brand.name,
+        brand: brand.name
       }
     } catch(err) {
-      if (!err.response) throw err
-      if (err.response.statusCode === 404) {
-        throw httpErrors.notFound()
-      }
-      if (err.response.statusCode === 400) {
-        throw httpErrors.badRequest()
-      }
-      throw err
+      if (!err.response) return reply.send(err)
+      if (err.response.statusCode === 404) return reply.code(404).send('Not Found')
+      if (err.response.statusCode === 400) return reply.code(400).send('Bad Request')
+      return reply.send(err)
     }
   })
 }
