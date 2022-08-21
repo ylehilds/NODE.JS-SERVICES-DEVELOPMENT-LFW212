02aa9d74891f9b17f0e80cc8260aa3a2e73a4ab0
ylehilds <lehi_alcantara@byu.edu>
2022-08-20 18:20:22 -0600

    added lab steps.

---
+++ Resources/labs/ch-9/labs-2/steps.js
@@ -0,0 +1,31 @@
+// how to complete this lab:
+// #1 inside fastify configuration create an object variable to hold the schema rule, call it bodySchema
+// #2 remember trap:
+// t - type
+// r - required
+// a - additionalProperties
+// p - properties
+// it wil look like this:
+const bodySchema = {
+  type: 'object',
+  required: ['data'],
+  additionalProperties: false,
+  properties: {
+    data: {
+      type: 'object',
+      required: ['brand', 'color'],
+      additionalProperties: false,
+      properties: {
+        brand: {type: 'string'},
+        color: {type: 'string'}
+      }
+    }
+  }
+}
+// #3 add the schema rule in the post method you want the rule in between first (path) and seconf parameter (function handler) like this:
+fastify.post('/', {
+  schema: {
+    body: bodySchema
+  }
+}, async (request, reply) => {
+...
