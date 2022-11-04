39b586affb3a4f4011e65c344432c95564a9bafd
ylehilds <lehi_alcantara@byu.edu>
2022-11-03 18:50:09 -0600

    added for exam purposes if you needed to wait 1 second before responding in comments on lab 3.1.

--- Resources/labs/ch-3/labs-1/routes/root.js
+++ Resources/labs/ch-3/labs-1/routes/root.js
@@ -3,5 +3,8 @@ const data = require('../data')
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return reply.code(200).send(await data())
+    // if you needed for exam purpose to wait 1 second before responding you could do it like this:
+    // setTimeout(async () => {return reply.code(200).send(await data())}, 1000)
+    // await reply
   })
 }
